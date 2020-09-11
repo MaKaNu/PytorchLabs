d53ff0fb8927f9318c23b959803c08d0555fb4bd
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2020-09-11 16:40:18 +0200

    Add Install Script

---
+++ setup.py
@@ -0,0 +1,19 @@
+""" Install Script for Package SemanticSegmentation """
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+setup(
+    name='SemanticSegmentation',
+    version='0.0.1',
+    author="MaKaNu",
+    url="https://github.com/MaKaNu/PytorchLabs/SemanticSegmentation",
+    packages=find_packages()
+)
+
+setup(
+    name="Datasets",
+    version="0.0.1",
+    author="MaKaNu",
+    url="https://github.com/MaKaNu/PytorchLabs/Datasets",
+    packages=find_packages()
+)
