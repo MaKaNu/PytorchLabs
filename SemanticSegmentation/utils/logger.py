083a3296f18f7f6968e4bed4ea21e635dc3cd37a
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2020-09-28 11:47:58 +0200

    Add Docstring to longmsg

--- SemanticSegmentation/utils/logger.py
+++ SemanticSegmentation/utils/logger.py
@@ -26,6 +26,16 @@ class CustomFormatter(logging.Formatter):
 
     @staticmethod
     def longmsg(msg):
+        """tries to slice a str after step elements, if it is a space_element.
+        if it is not a space element the str will be sliced at the last
+        space_element before the step elements
+
+        Args:
+            msg (str): Message as a string.
+
+        Returns:
+            list with all the elements with len < step
+        """
         assert isinstance(msg, str)
         step = 80
         final_msg = []
