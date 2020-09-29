73741d2e3c9cc3178c5dc46ca004e041885bb0a0
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2020-09-29 16:56:37 +0200

    Continue train process

--- SemanticSegmentation/train/fcn/train.py
+++ SemanticSegmentation/train/fcn/train.py
@@ -5,20 +5,27 @@
     and probably more
 """
 from __future__ import absolute_import
-from logging import log # TODO
-from os import abort # TODO
+
+from datetime import datetime
+import random
+
 import sys
 import logging
 from pathlib import Path
 from importlib import import_module
 import torch
+from torch import optim
+from torch.autograd import Variable
 from torch.backends import cudnn
-from torch.nn.modules.loss import TripletMarginLoss
-from torch.utils import data
+from torch.nn.modules.loss import CrossEntropyLoss
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+import torchvision.utils as vutils
 
 from SemanticSegmentation.utils.logger import CustomFormatter as CF
 from SemanticSegmentation.utils.hyperparams import SysArgWrapper, EnvParams, \
     TrainParams, LogLevel
+from SemanticSegmentation.utils.misc import check_mkdir, evaluate, AverageMeter
 
 from SemanticSegmentation.models.fcn8 import FCN8
 
@@ -65,7 +72,7 @@ def main(argv):
 
     try:
         dataset = import_module('Datasets.' + env_args['dataset'])
-        logger.info(repr(dataset))
+        logger.debug(repr(dataset))
     except ImportError as err:
         print('Error:', err)
         dataset = None
@@ -74,14 +81,14 @@ def main(argv):
         net = FCN8(num_classes=dataset.NUM_CLASSES).cuda()
 
     # Check if Training should Continue at certain Snapshot and if load it.
+        ckpt_path, exp_name = env.checkpt_path, env.export_name
         if len(train_args['snapshot'])==0:
             curr_epoch = 1
             train_args['best_record'] = {
                 'epoch': 0, 'val_loss': 1e10, 'acc': 0, 'acc_cls': 0,
                 'mean_iu': 0, 'fwavacc': 0}
         else:
-            print('training resumes from ' + train_args['snapshot'])
-            ckpt_path, exp_name = env.checkpt_path, env.export_name
+            logger.info('training resumes from ' + train_args['snapshot'])
             net.load_state_dict(torch.load(ckpt_path / Path(exp_name) / Path(train_args['snapshot'])))
             split_snapshot = train_args['snapshot'].split('_')
             curr_epoch = int(split_snapshot[1]) + 1
@@ -104,8 +111,8 @@ def main(argv):
             test_count = count_image - train_count - valid_count
             assert count_image == (train_count + valid_count + test_count)
 
-            dataset.Dataset.tranform = dataset.input_transform
-            dataset.Dataset.target_tranform = dataset.target_transform
+            dataset.Dataset.transform = dataset.input_transform
+            dataset.Dataset.target_transform = dataset.target_transform
 
             trainset, validset, testset = torch.utils.data.random_split(
                 dataset.Dataset, (train_count, valid_count, test_count)
@@ -113,13 +120,176 @@ def main(argv):
 
             logger.debug(str((len(trainset), len(validset), len(testset))))
         else:
+            validset = None
+            trainset = None
             logger.critical('NOT IMPLEMENTED!')
 
+        train_loader = DataLoader(
+            trainset, batch_size=1, num_workers=0, shuffle=True)
+        val_loader = DataLoader(
+            validset, batch_size=1, num_workers=0, shuffle=False)
 
+        criterion = CrossEntropyLoss(
+            reduction='sum', ignore_index=dataset.IGNORE_LABEL).cuda()
+
+        optimizer = optim.Adam([
+            {'params': [
+        param for name, param in net.named_parameters() if name[-4:] == 'bias'],
+             'lr': 2 * train_args['learn_rate']},
+            {'params': [
+        param for name, param in net.named_parameters() if name[-4:] != 'bias'],
+            'lr': train_args['learn_rate'], 
+            'weight_decay': train_args['weight_decay']}
+        ], betas=(train_args['momentum'], 0.999))
 
         
+        if len(train_args['snapshot']) > 0:
+            optimizer.load_state_dict(torch.load(
+                ckpt_path / Path(exp_name) / \
+                Path('opt_' + train_args['snapshot'])))
+            optimizer.param_groups[0]['lr'] = 2 * train_args['lr']
+            optimizer.param_groups[1]['lr'] = train_args['lr']
+
+        check_mkdir(ckpt_path)
+        check_mkdir(ckpt_path / Path(exp_name))
+        actual_time = '_'.join(str(datetime.now()).split())
+        actual_time = '_'.join(actual_time.split(':'))
+        actual_time = '_'.join(actual_time.split('.'))
+        # actual_time = 'file'
+        ckpt_file = ckpt_path / Path(exp_name) / Path(actual_time + '.txt')
+        ckpt_file.write_text(str(train_args) + '\n\n')
+
+        scheduler = ReduceLROnPlateau(
+            optimizer, 'min',
+            patience=train_args['lr_patience'],
+            min_lr=1e-10, verbose=True)
+        for epoch in range(curr_epoch, train_args['epoch_num'] + 1):
+            train(
+                train_loader, net, criterion, optimizer, epoch, train_args, 
+                env, dataset.NUM_CLASSES)
+            val_loss = validate(
+                val_loader, net, criterion, optimizer, epoch, train_args, 
+                dataset.restore_transform, dataset.visualize, env, logger)
+            scheduler.step(val_loss)
+
+def train(train_loader, net, criterion, optimizer,
+          epoch, train_args, env, num_classes):
+    train_loss = AverageMeter()
+    curr_iter = (epoch - 1) * len(train_loader)
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data['image'], data['label']
+        assert inputs.size()[2:] == labels.size()[1:3]
+        N = inputs.size(0)
+        inputs = Variable(inputs).cuda()
+        labels = Variable(labels).cuda()
+
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        assert outputs.size()[2:] == labels.size()[1:3]
+        assert outputs.size()[1] == num_classes
+
+        loss = criterion(outputs, labels) / N
+        loss.backward()
+        optimizer.step()
+
+        train_loss.update(loss.data[0], N)
+
+        curr_iter += 1
+        env.writer.add_scalar('train_loss', train_loss.avg, curr_iter)
+
+        if (i + 1) % train_args['print_freq'] == 0:
+            print('[epoch %d], [iter %d / %d], [train loss %.5f]' % (
+                epoch, i + 1, len(train_loader), train_loss.avg
+            ))
 
 
+def validate(
+    val_loader, net, criterion, optimizer, epoch, 
+    train_args, restore, visualize, env, logger):
+
+    net.eval()
+
+    ckpt_path, exp_name = env.checkpt_path, env.export_name
+
+    val_loss = AverageMeter()
+    inputs_all, gts_all, predictions_all = [], [], []
+
+    for _, data in enumerate(val_loader):
+        inputs, gts = data
+        N = inputs.size(0)
+        inputs = Variable(inputs, volatile=True).cuda()
+        gts = Variable(gts, volatile=True).cuda()
+
+        outputs = net(inputs)
+        predictions = outputs.data.max(1)[1].squeeze_(1).squeeze_(0).cpu().numpy()
+
+        val_loss.update(criterion(outputs, gts).data[0] / N, N)
+
+        if random.random() > train_args['val_img_sample_rate']:
+            inputs_all.append(None)
+        else:
+            inputs_all.append(inputs.data.squeeze_(0).cpu())
+        gts_all.append(gts.data.squeeze_(0).cpu().numpy())
+        predictions_all.append(predictions)
+
+    acc, acc_cls, mean_iu, fwavacc = evaluate(
+        predictions_all, gts_all, dataset.num_classes)
+
+    if mean_iu > train_args['best_record']['mean_iu']:
+        train_args['best_record']['val_loss'] = val_loss.avg
+        train_args['best_record']['epoch'] = epoch
+        train_args['best_record']['acc'] = acc
+        train_args['best_record']['acc_cls'] = acc_cls
+        train_args['best_record']['mean_iu'] = mean_iu
+        train_args['best_record']['fwavacc'] = fwavacc
+        snapshot_name = 'epoch_%d_loss_%.5f_acc_%.5f_acc-cls_%.5f_mean-iu_%.5f_fwavacc_%.5f_lr_%.10f' % (
+            epoch, val_loss.avg, acc, acc_cls, mean_iu, fwavacc, optimizer.param_groups[1]['lr']
+        )
+        torch.save(net.state_dict(), ckpt_path / Path(exp_name) / \
+            Path(snapshot_name + '.pth'))
+        torch.save(optimizer.state_dict(), ckpt_path / Path(exp_name) / \
+            Path('opt_' + snapshot_name + '.pth'))
+
+        to_save_dir = ckpt_path / Path(exp_name) /  Path(str(epoch))
+        if train_args['val_save_to_img_file']:
+            check_mkdir(to_save_dir)
+
+        val_visual = []
+        for idx, data in enumerate(zip(inputs_all, gts_all, predictions_all)):
+            if data[0] is None:
+                continue
+            input_pil = restore(data[0])
+            gt_pil = dataset.colorize_mask(data[1])
+            predictions_pil = dataset.colorize_mask(data[2])
+            if train_args['val_save_to_img_file']:
+                input_pil.save(to_save_dir / Path('%d_input.png' % idx))
+                predictions_pil.save(to_save_dir / Path('%d_prediction.png' % idx))
+                gt_pil.save(to_save_dir / Path('%d_gt.png' % idx))
+            val_visual.extend([visualize(input_pil.convert('RGB')), visualize(gt_pil.convert('RGB')),
+                               visualize(predictions_pil.convert('RGB'))])
+        val_visual = torch.stack(val_visual, 0)
+        val_visual = vutils.make_grid(val_visual, nrow=3, padding=5)
+        env.writer.add_image(snapshot_name, val_visual)
+
+    logger.info(68 * '-')
+    logger.info('[epoch %d], [val loss %.5f], [acc %.5f], [acc_cls %.5f], [mean_iu %.5f], [fwavacc %.5f]' % (
+        epoch, val_loss.avg, acc, acc_cls, mean_iu, fwavacc))
+
+    logger.info('best record: [val loss %.5f], [acc %.5f], [acc_cls %.5f], [mean_iu %.5f], [fwavacc %.5f], [epoch %d]' % (
+        train_args['best_record']['val_loss'], train_args['best_record']['acc'], train_args['best_record']['acc_cls'],
+        train_args['best_record']['mean_iu'], train_args['best_record']['fwavacc'], train_args['best_record']['epoch']))
+
+    logger.info(68 * '-')
+
+    env.writer.add_scalar('val_loss', val_loss.avg, epoch)
+    env.writer.add_scalar('acc', acc, epoch)
+    env.writer.add_scalar('acc_cls', acc_cls, epoch)
+    env.writer.add_scalar('mean_iu', mean_iu, epoch)
+    env.writer.add_scalar('fwavacc', fwavacc, epoch)
+    env.writer.add_scalar('lr', optimizer.param_groups[1]['lr'], epoch)
+
+    net.train()
+    return val_loss.avg
 
 if __name__ == '__main__':
     main(sys.argv)
