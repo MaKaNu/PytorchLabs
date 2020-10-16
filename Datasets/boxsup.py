b75c90a3011d4de76d6b89006c84c6c5f88fe39e
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2020-10-16 15:31:48 +0200

    FIX PEP235

--- Datasets/boxsup.py
+++ Datasets/boxsup.py
@@ -1,7 +1,7 @@
 """ This Module creates Dataset based on the BoxSupDataset Package
 """
 from __future__ import absolute_import
-from BoxSupDataset.nasa_box_sup_dataset import NasaBoxSupDataset
+from boxsupdataset.nasa_box_sup_dataset import NasaBoxSupDataset
 from numpy.core.fromnumeric import mean
 from torchvision import transforms
 from PIL import Image
